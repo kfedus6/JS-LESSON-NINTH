233fed1e3ca532fa2aa05a49c839deb208dc14af
kfedus6 <feduskolya@gmail.com>
2022-01-12 15:55:48 +0200

    Sample code

--- page.js
+++ page.js
@@ -1,6 +1,51 @@
-//dz
-const table = document.querySelector('#table');
+/*
+text.onchange = (e) => {
+   console.log('change');
+}
+*/
+/*
 
-table.addEventListener('click', (e) => {
+text.oninput = () => {
+   if (text.value.length > 10) {
+      let s;
+      s = text.value.substring(0, 10);
+      text.value = s;
+   }
+   
+}
+
+/*
+let s = "hello world"
+s = s.substring(0, 6);
+console.log(s)
+*/
+/*
+let sel = document.querySelector('select');
+sel.onchange = (e) => {
    console.log(e.target);
-})
+}
+
+text.oncopy = text.oncut = text.onpaste = (e) => {
+   console.log(e.type)
+   console.log(e.clipboardData.getData('text/plain'))
+}
+
+let num = document.querySelector('.number');
+num.onpaste = (e) => {
+   if (isNaN(Number(e.clipboardData.getData('text/plain'))) == true) {
+      console.log(isNaN(Number(num.value)))
+      return false;
+   }
+   
+}
+
+const s = "31231"
+console.log(isNaN(Number(s)))
+*/
+/*
+let text = document.querySelector(".text");
+
+text.oninput = (e) => {
+   document.querySelector('.number').value = text.value
+}
+*/
